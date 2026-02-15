322833ea7316391aa7b41b998b9f772168abb93c
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2026-02-14 23:01:17 -0800

    using modern shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,28 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// Type for server action results
-export type ActionResult<T> = {
-  success: boolean;
-  data?: T;
-  error?: string;
-}
-
-// Helper function to return a successful result
-export function success<T>(data: T): ActionResult<T> {
-  return {
-    success: true,
-    data
-  }
-}
-
-// Helper function to return a failure result
-export function failure(error: unknown , clientError?: string): ActionResult<never> {
-  const errorString = error instanceof Error ? error.message : error as string;
-  console.log("error: ", errorString);
-  return {
-    success: false,
-    error: clientError || errorString
-  }
-}
